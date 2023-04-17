4a6f8113eaa6bd8d47fc383657c27819fae79062
shabbir <talkin0x10@gmail.com>
2023-04-17 22:30:38 +0530

    deployed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MainComponent } from './main/main.component';
+import { ButtonComponent } from './button/button.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    MainComponent,
+    ButtonComponent,
+
   ],
   imports: [
     BrowserModule
